b1a784fe90202fcf11c8a82d347360a8b877efbe
kdaud <dkakumirizii@gmail.com>
2019-12-31 22:56:59 +0300

    junit

---
+++ src/com/junit/tests/Beginners.java
@@ -0,0 +1,10 @@
+package com.junit.tests;
+
+
+public class Beginners {
+	
+	public String concatinate(String fname, String lname) {
+		return fname + " " + lname;
+	}
+	
+}
---
+++ src/com/junit/tests/Testing.java
@@ -0,0 +1,13 @@
+package com.junit.tests;
+
+
+public class Testing {
+	
+	public static void main(String[] args) {
+		Beginners obj = new Beginners();
+		String name = obj.concatinate("Magari", "Godfrey");
+		System.out.println(name);
+		System.out.println(obj.concatinate("Aidah", "Nassali"));
+	}
+	
+}

79e7cb3b0767b96dfab044fa9f0486645fd3a673
kdaud <dkakumirizii@gmail.com>
2020-01-02 15:48:02 +0300

    files

--- src/tests/District.java
+++ src/tests/District.java
@@ -1,5 +1,8 @@
 package tests;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class District {
 	
@@ -9,6 +12,20 @@ public class District {
 		System.out.println(obj.toString());
 		System.out.println(obj.hashCode());
 		System.out.println(obj.getClass().getName());
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter your age:");
+		try {
+			Integer.parseInt(br.readLine());
+		}
+		catch (NumberFormatException | IOException e) {
+			
+			System.err.println("Error");
+		}
+		
+		finally {
+			System.out.println("Congratualation");
+		}
 	}
 	
 }

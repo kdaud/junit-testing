d26e0623481698810a3246476c7e7e86466e7489
kdaud <dkakumirizii@gmail.com>
2020-01-02 15:41:30 +0300

    files

--- src/tests/District.java
+++ src/tests/District.java
@@ -5,9 +5,10 @@ public class District {
 	
 	public static void main(String[] args) {
 		Farmer obj = new Farmer("Kafeero", "Central", 30, "Peasant Farmer");
-		System.out.println(obj.getAge());
-		System.out.println("Name: " + obj.getName());
-		System.out.println("Central: " + obj.getRegion());
+		
+		System.out.println(obj.toString());
+		System.out.println(obj.hashCode());
+		System.out.println(obj.getClass().getName());
 	}
 	
 }
